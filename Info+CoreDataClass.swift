85ddb6650a8e339253ba703d220c1c93fd07f6c2
mobapp08 <mobapp08@dt-c202-m05.local>
2017-03-14 14:42:54 +0100

    commit eke

---
+++ Info+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Info+CoreDataClass.swift
+//  AccesSEAble
+//
+//  Created by mobapp08 on 14/03/17.
+//  Copyright © 2017 jo van kriekingen. All rights reserved.
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+@objc(Info)
+public class Info: NSManagedObject {
+
+}
