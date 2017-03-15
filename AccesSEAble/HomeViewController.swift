104762fcfef9f6a7f076cf141213ce8bc35d125f
Lise De Ridder <lise_de_ridder@MacBook-Pro-van-Lise.local>
2017-03-15 06:04:41 +0100

    parsers app, poi en tram

--- AccesSEAble/HomeViewController.swift
+++ AccesSEAble/HomeViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
     // Do any additional setup after loading the view, typically from a nib.
   }
 
--- AccesSEAble/InfoTramLogiesDAO.swift
+++ AccesSEAble/InfoTramLogiesDAO.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+import CoreData
+
+
+public class tramDAO {
+    
+    func parseTram (context:NSManagedObjectContext)
+    {
+        
+        let url = URL(string: "http://web10.weopendata.com/measurements/tram.json")
+        do {
+            
+            let tramData = try Data(contentsOf: url!)
+            let tramArray:NSArray = try JSONSerialization.jsonObject(with: tramData) as! NSArray
+            
+            
+            for item in tramArray {
+                
+                let tramMember:NSDictionary = item as! NSDictionary
+                
+                let rowTram = Tram(context: context)
+                
+                
+                rowTram.stop_id = tramMember.value(forKey: "stop_id") as? String
+                rowTram.stop_code = tramMember.value(forKey: "stop_code") as? String
+                rowTram.naam = tramMember.value(forKey: "stop_name") as? String
+                rowTram.stop_desc = tramMember.value(forKey: "stop_desc") as? String
+                rowTram.stop_lat = tramMember.value(forKey: "stop_lat") as? String
+                rowTram.stop_lon = tramMember.value(forKey: "stop_lon") as? String
+                rowTram.zone_id = tramMember.value(forKey: "zone_id") as? String
+                rowTram.stop_url = tramMember.value(forKey: "stop_url") as? String
+                rowTram.location_type = tramMember.value(forKey: "location_type") as? String
+                rowTram.parent_station = tramMember.value(forKey: "parent_station") as? String
+                rowTram.weelchair_boarding = tramMember.value(forKey: "weelchair_boarding") as? String
+                print(tramMember)
+                
+            }
+        } catch  {
+            print("parse error tram")
+            
+        }
+
+
+    }
